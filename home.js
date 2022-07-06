7f7895caf57f330116ad99c8191a83d89bf9ed0b
Margaret <margaret.r.stern@gmail.com>
2022-07-06 10:37:43 -0400

    connected both pages

--- home.js
+++ home.js
@@ -1,1 +1,6 @@
 console.log('this is connected')
+// const button = document.querySelector(`#startGamePlay`)
+
+// button.addEventListener('click', () => {
+//   document.body.location.href = `game.html`
+// })
